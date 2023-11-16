08bd3bf8c39da16dfc9ebc75e53c487f757f5b7c
Danilo Melo <danilopires.melo@gmail.com>
2023-11-16 13:47:39 -0300

    imprementada a função comprar pão françês

--- projeto_comprar_pao/app_comprar_pao/templates/usuarios/comprar.js
+++ projeto_comprar_pao/app_comprar_pao/templates/usuarios/comprar.js
@@ -1,9 +1,5 @@
-script type="text/javascript">
-  function adicionar(produto, preco) {
-    var quantidade = prompt("Digite a quantidade de " + produto + " que deseja comprar:");
-    if (quantidade != null && quantidade > 0) {
-      var total = quantidade * preco;
-      alert("O valor total da sua compra é R$ " + total.toFixed(2));
-    }
-  }
-</script>
+
+  def adicionar(produto, preco) {
+  print(bateu);
+
+}
