0dbe4b2021e785a65ddeb5ce7d178b2739dd2329
pmdanilo <danilopires.melo@gmail.com>
2023-11-24 21:43:56 -0300

    Removido o arquivo de CSS e Javascript, ambos estão integrado no HTML

--- projeto_comprar_pao/projeto_comprar_pao/urls.py
+++ projeto_comprar_pao/projeto_comprar_pao/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from app_comprar_pao import views
 
 urlpatterns = [
-    #rota, view responsável, nome de referência
-    path('',views.home,name='home'),
+        path('',views.home,name='home'),
 ]
